496f97da7d6d1d4daf354d88e2248f5bd725b8fd
0173916 <1257668543@qq.com>
2020-07-29 21:28:54 +0800

    三元云音乐 web->小程序仿写

--- js/wxapp/doubanMovie/pages/home/home.js
+++ js/wxapp/doubanMovie/pages/home/home.js
@@ -9,10 +9,27 @@ Page({
     inputVal: ""
   },
   onLoad() {
+      this.getCity((city) => {
+        console.log(city)
+        this.loadData(0, {city: city, apikey: '0df993c66c0c636e29ecbb5344252a4a'})
+      })
       this.setData({
           search: this.search.bind(this)
       })
   },
+
+  loadData(idx, params) {
+    let url = wx.db.url('/v2/movie/in_theaters') 
+    wx.request({
+        url: url,
+        data: params,
+        header: {'content-type': 'json'},
+        success: (res) => {
+            console.log(res)
+        }
+    })
+  },
+
   search: function (value) {
       return new Promise((resolve, reject) => {
           setTimeout(() => {
@@ -22,6 +39,38 @@ Page({
   },
   selectResult: function (e) {
       console.log('select result', e.detail)
+  },
+
+  getCity(succeed) {
+    // 拿到当前所在的城市名称
+    // 先拿到当前所在区域的经纬度（小程序自带API）
+    wx.getLocation({
+        type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+        success: function(res){
+            // console.log(res)
+            // 将经纬度转化成具体的名称
+            wx.request({
+                url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+                data: {
+                  ak: 'uXDG6lrpKFg36aUkGnzzcrizuPfTdcVH',
+                  output: 'json',
+                  coordtype: 'wgs84ll',
+                  location: `${res.latitude},${res.longitude}`
+                },
+                success: (res) => {
+                    console.log(res)
+                    let city = res.data.result.addressComponent.city
+                    succeed && succeed(city)
+                },
+                fail: () => {
+                    console.log('获取地理信息失败')
+                }
+            })
+        },
+        fail: function() {
+            // fail
+        },
+    })
   },
 
   /**
