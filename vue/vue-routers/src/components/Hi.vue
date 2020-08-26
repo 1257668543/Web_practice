f7b36a98eb19d0174304726c8902d1d1bca43e2b
0173916 <1257668543@qq.com>
2020-08-26 12:06:26 +0800

    vue路由

--- vue/vue-routers/src/components/Hi.vue
+++ vue/vue-routers/src/components/Hi.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="hello">
-      <h1>{{msg}}</h1>
-      <router-link to="/hi">hi</router-link>
-      <span style="display: inline-block; width: 50px"></span>
-      <router-link :to="{name:'hi1', params:{name: '乐怡'}}">hi 一</router-link>
-      <span style="display: inline-block; width: 50px"></span>
-      <router-link to="/hi/hi2">hi 二</router-link>
-      <router-view></router-view>
+    <h1>{{msg}}</h1>
+    <router-link to="/hi">hi</router-link>
+    <span style="display: inline-block; width: 50px"></span>
+    <router-link :to="{name:'hi1', params:{name: '乐怡'}}">hi 一</router-link>
+    <span style="display: inline-block; width: 50px"></span>
+    <router-link to="/hi/hi2">hi 二</router-link>
+    <span style="display: inline-block; width: 50px"></span>
+    <router-link to="/abc">404</router-link>
+    <div>
+        {{ $route.params.name }}
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -17,6 +22,14 @@ export default {
         return {
             msg: 'Hi, I am wn'
         }
+    },
+    beforeRouteEnter: (to, from, next) => {
+        console.log('准备进入目标')
+        next()
+    },
+    beforeRouteLeave: (to, from, next) => {
+        console.log('准备离开目标')
+        next()
     }
 }
 </script>
--- vue/vue-routers/src/router/index.js
+++ vue/vue-routers/src/router/index.js
@@ -3,8 +3,9 @@ import Vue from 'vue'
 import router from 'vue-router'
 import Hello from '../components/HelloWorld'
 import Hi from '../components/Hi'
-import Hi1 from '../components/Hi1'
+// import Hi1 from '../components/Hi1'
 import Hi2 from '../components/Hi2'
+import Error from '@/components/Error'
 
 Vue.use(router)
 
@@ -13,23 +14,42 @@ export default new router({
     {
       path: '/', 
       name: 'Hello',
-      component: Hello
+      component: Hello,
     },
+    // {
+    //   path: '*',
+    //   component: Error
+    // },
     {
         path: '/hi', 
         name: 'Hi',
         component: Hi,
         children: [
             {
-                path: 'hi1', 
-                name: 'hi1', 
-                component: Hi1},//子路由的<router-view>必须在HelloWorld.vue中出现
+              path: 'hi1', 
+              name: 'hi1', 
+              // component: Hi1,
+              redirect: '/' // 路由重定向 
+            },//子路由的<router-view>必须在HelloWorld.vue中出现
             {
-                path: 'hi2', 
-                name: 'hi2', 
-                component: Hi2
+              path: 'hi2', 
+              name: 'hi2', 
+              component: Hi2,
+              alias: 'ac' // 别名
             }
         ]
+    },
+    {
+      path: '/abc',
+      name: 'abc',
+      component: Error,
+      beforeEnter: (to, from, next) => {
+        console.log('我进入了404页面')
+        console.log(to)
+        console.log(from)
+        console.log(to.params)
+        next()
+      }
     }
   ]  
 })
