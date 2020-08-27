fb8b36f174b9417041b50f1b1cb3690fd947cdbc
0173916 <1257668543@qq.com>
2020-08-27 12:02:41 +0800

    vue组件

---
+++ vue/vue-component/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+      <p>{{userName}}</p>
+      <!-- <button @click="handle">按钮</button> -->
+      <button @click="changBaby">养新的宠物</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['userName'],
+    data() {
+        return {
+            name: 'wn',
+            title: '我是子组件里的title',
+            baby: '大蜗牛'
+        }
+    },
+    methods: {
+        handle() {
+            // console.log('ok')
+            this.$emit('titleChange', this.title)
+        },
+        changBaby() {
+            this.$emit('babyChange', this.baby)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-component/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <p>{{title}}</p>
+    <User :userName="name" @titleChange="updateTitle"></User>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import User from '@/components/User'
+export default {
+  name: 'Home',
+  data() {
+    return {
+      name: 'wl',
+      title: ''
+    }
+  },
+  components: {
+      User
+  },
+  methods: {
+    updateTitle(e) {
+      // console.log('接受到了来自子组件的数据--' + e)
+      this.title = e
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
