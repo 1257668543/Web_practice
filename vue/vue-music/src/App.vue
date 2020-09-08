cb3d652ecc5c33bc4535a1da8314c70516c22895
0173916 <1257668543@qq.com>
2020-09-08 17:12:24 +0800

    vue-music项目

--- vue/vue-music/src/App.vue
+++ vue/vue-music/src/App.vue
@@ -1,25 +1,55 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header>
+      <i class="iconfont" slot="left-icon">&#xe700;</i>
+      <span slot="content">网抑云音乐</span>
+      <i class="iconfont" slot="right-icon">&#xe90e;</i>
+    </v-header>
     <!-- <router-view/> -->
+
+    <!-- menu -->
+  <v-sidebar></v-sidebar>
   </div>
 </template>
 
 <script>
 import header from '@/components/header'
+import sidebar from '@/components/sidebar'
 export default {
   components: {
-    'v-header': header
+    'v-header': header,
+    'v-sidebar': sidebar
   }
 }
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+@font-face {
+  font-family: 'iconfont';  /* project id 2052921 */
+  src: url('//at.alicdn.com/t/font_2052921_6p1msre3huk.eot');
+  src: url('//at.alicdn.com/t/font_2052921_6p1msre3huk.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.woff') format('woff'),
+  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.svg#iconfont') format('svg');
+}
+.iconfont{
+  font-family:"iconfont" !important;
+  font-size:16px;font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
+html,
+body {
+  line-height: 1;
+  font-family: PingFang SC, STHeitiSC-Light, Helvetica-Light, arial, sans-serif;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  background: rgba(8, 5, 58, 0.9);
+  color: #fff;
+}
 </style>
