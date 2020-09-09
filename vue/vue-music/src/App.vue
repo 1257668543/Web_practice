6c61edbf7950819613cbb6c45fb996608c887e21
0173916 <1257668543@qq.com>
2020-09-09 17:05:50 +0800

    vue-music tabBar与搜索页面

--- vue/vue-music/src/App.vue
+++ vue/vue-music/src/App.vue
@@ -5,7 +5,11 @@
       <span slot="content">网抑云音乐</span>
       <i class="iconfont" slot="right-icon">&#xe90e;</i>
     </v-header>
-    <!-- <router-view/> -->
+    <v-tab></v-tab>
+
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
 
     <!-- menu -->
   <v-sidebar></v-sidebar>
@@ -15,10 +19,12 @@
 <script>
 import header from '@/components/header'
 import sidebar from '@/components/sidebar'
+import tab from '@/components/tab'
 export default {
   components: {
     'v-header': header,
-    'v-sidebar': sidebar
+    'v-sidebar': sidebar,
+    'v-tab': tab
   }
 }
 </script>
@@ -26,12 +32,12 @@ export default {
 <style lang="stylus">
 @font-face {
   font-family: 'iconfont';  /* project id 2052921 */
-  src: url('//at.alicdn.com/t/font_2052921_6p1msre3huk.eot');
-  src: url('//at.alicdn.com/t/font_2052921_6p1msre3huk.eot?#iefix') format('embedded-opentype'),
-  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.woff2') format('woff2'),
-  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.woff') format('woff'),
-  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.ttf') format('truetype'),
-  url('//at.alicdn.com/t/font_2052921_6p1msre3huk.svg#iconfont') format('svg');
+  src: url('//at.alicdn.com/t/font_2052921_vs3vng9a86.eot');
+  src: url('//at.alicdn.com/t/font_2052921_vs3vng9a86.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_2052921_vs3vng9a86.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_2052921_vs3vng9a86.woff') format('woff'),
+  url('//at.alicdn.com/t/font_2052921_vs3vng9a86.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_2052921_vs3vng9a86.svg#iconfont') format('svg');
 }
 .iconfont{
   font-family:"iconfont" !important;
