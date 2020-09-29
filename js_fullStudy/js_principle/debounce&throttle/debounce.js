7e5ed2a927127d5a38cab5d42d9269316f53b933
0173916 <1257668543@qq.com>
2020-09-29 16:06:08 +0800

    防抖节流原理实现

---
+++ js_fullStudy/js_principle/debounce&throttle/debounce.js
@@ -0,0 +1,21 @@
+// 在规定的时间内未触发第二次，则执行
+function debounce(func, delay) {
+  // 利用闭包保存定时器
+  let timer = null
+  return function() {
+    // 在规定的时间内再次触发会清除定时器后再重设定时器
+    clearTimeout(timer) 
+    timer = setTimeout(() => {
+      func.apply(this, arguments)
+    }, delay)
+  }
+}
+
+window.onscroll = debounce(function() {
+  console.log('scroll')
+}, 1000)
+
+xxx.addEventListener('click', debounce(fn, 1000))
+function fn() {
+  console.log('wl')
+}
