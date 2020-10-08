51a7cfa9937c5eaa8fc788ed8f26fda50996cff7
0173916 <1257668543@qq.com>
2020-10-08 23:14:25 +0800

    小米商城项目开始

---
+++ vue/vue-mi/mimall-client/src/views/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-wrap">
+    <div class="m-site-top">
+      <div class="container clearfix">
+        <div class="fr">
+          <div class="fl site-item m-user-con">
+            <div class="m-no-login">
+              <a href="https://account.xiaomi.com/pass/serviceLogin?sid=miotstore&_bannerBiz=miotstore&callback=https%3A%2F%2Fshopapi.io.mi.com%2Fapp%2Fshop%2Fauth%3Flogid%3D1602169190.811269641%26sign%3Db3d43fa56c93f5725a4d7e0825f8cb0a%26followup%3Dhttps%253A%252F%252Fwww.xiaomiyoupin.com%252F%253Fopapp%253D5%2526source%253Dyiqifa_19651%25257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%25253D%25253D_3.yiqifa.19651%25257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%25253D%25253D.1602252595199" class="m-safe-anchor">登录</a>
+              <a href="https://cn.account.xiaomi.com/pass/register?followup=https%3A%2F%2Fwww.xiaomiyoupin.com%2F%3Fopapp%3D5%26source%3Dyiqifa_19651%257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%253D%253D_3.yiqifa.19651%257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%253D%253D.1602255603390" class="m-safe-anchor">注册</a>
+            </div>
+          </div>
+          <span class="fl m-line"></span>
+          <div class="fl m-help"></div>
+          <span class="fl m-line"></span>
+          <div class="fl m-download site-item"></div>
+          <span class="fl m-line"></span>
+          <div class="fl m-clauses-con"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
