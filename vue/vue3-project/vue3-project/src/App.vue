1b681a69b5e58dcd6263826398db386370b4467a
0173916 <1257668543@qq.com>
2020-10-14 21:11:42 +0800

    vue3.0项目

--- vue/vue3-project/vue3-project/src/App.vue
+++ vue/vue3-project/vue3-project/src/App.vue
@@ -9,51 +9,66 @@
         :style="{ lineHeight: '64px' }"
       >
         <a-menu-item key="/">
-          <router-link to="/">首页</router-link> 
+          <router-link to="/">首页</router-link>
         </a-menu-item>
         <a-menu-item key="/plan">
-          <router-link to="/plan">计划</router-link> 
+          <router-link to="/plan">计划</router-link>
         </a-menu-item>
       </a-menu>
     </a-layout-header>
     <a-layout-content style="padding: 0 50px">
       <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
-
-        <router-view></router-view>
+        <a-row>
+          <a-col :span="6">
+            <a-card title="Default size card" style="width: 300px">
+              <template v-slot:extra><a href="#">more</a></template>
+              <p>计划总时间：{{allTime}}</p>
+            </a-card>
+          </a-col>
+          <a-col :span="16" :offset="2">
+            <router-view></router-view>
+          </a-col>
+        </a-row>
       </div>
     </a-layout-content>
     <a-layout-footer style="text-align: center">
-      {{count}}
+      {{ count }}
     </a-layout-footer>
   </a-layout>
 </template>
 <script>
-// composition api 
-import { reactive, toRefs, computed } from 'vue'
-import { useRoute } from 'vue-router'
+// composition api
+import { reactive, toRefs, computed } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
 export default {
-  setup () { // 入口函数 默认只执行一次 里面没有this拿不到vue的实例
-    const route = useRoute()
+  setup() {
+    // 入口函数 默认只执行一次 里面没有this拿不到vue的实例
+    const route = useRoute();
+    const store = useStore();
     const state = reactive({
       // selectedKeys: [],
-      count: 0
-    })
+      count: 0,
+      selectedKeys: computed(() => {
+        return [route.path];
+      }),
+      allTime: store.getters.allTime,
+    });
     // watch(() => route.path, (newVal) => {
     //   state.selectedKeys = [newVal]
     // }, { immediate: true }) // immediate 立即执行
 
-    const selectedKeys = computed(() => {
-      return [route.path]
-    })
+    // const selectedKeys = computed(() => {
+    //   return [route.path]
+    // })
+
     return {
       // selectedKeys: state.selectedKeys,
       // count: state.count
       ...toRefs(state), // 保证数据是响应式的，且做了解构
-      selectedKeys
-    }
-
-    
-  }
+      // selectedKeys
+    };
+  },
 };
 </script>
 <style lang="less">
--- vue/vue3-project/vue3-project/src/views/plan/index.vue
+++ vue/vue3-project/vue3-project/src/views/plan/index.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    plan
+    <Create @handleplan="handleplan"></Create>
   </div>
 </template>
 
 <script>
+import Create from './create'
 export default {
+  components: {
+    Create
+  },
+  setup() {
+    const handleplan = (plan) => {
+      console.log(plan);
+    }
 
+    return {
+      handleplan
+    }
+  }
 }
 </script>
 
