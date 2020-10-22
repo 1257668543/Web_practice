323a2db74e55cd1cbdef5978e5be01af758b913b
0173916 <1257668543@qq.com>
2020-10-22 11:02:28 +0800

    myKoa

--- node/myKoa/lib/context.js
+++ node/myKoa/lib/context.js
@@ -1,4 +1,21 @@
+const { deserialize } = require("v8")
+
 let proto = {}
 
+function defineGetter(prop, name) { // prop代表对象，name代表对象上的属性
+  proto.__defineGetter__(name, function() { // 每个对象都有__defineGetter__方法
+    return this[prop][name] // url === this.request.url
+  })
+}
+function defineSetter(prop, name) {
+  proto.__defineSetter__(name, function(val) { // 每个对象都有__defineSetter__方法
+   this[prop][name] = val // 使用defineGetter('response', 'body') 代理后 body === response.body === val 修改 ctx.body === 修改 ctx.response.body 
+  })
+}
+
+defineGetter('request', 'url')
+defineGetter('request', 'path')
+defineGetter('response', 'body')
+defineSetter('response', 'body')
 
 module.exports = proto
--- node/myKoa/lib/request.js
+++ node/myKoa/lib/request.js
@@ -1,3 +1,14 @@
-let request = {}
+let url = require('url')
+let request = {
+  get url () {  // get 访问器修饰符 这样 ctx.request.url 就可以直接取值了，不用再调用
+    return this.req.url
+  },
+  get path() {  // ctx.request.path
+    return url.parse(this.req.url).pathname
+  },
+  get query() {
+    return url.parse(this.req.url).query
+  }
+}
 
 module.exports = request
