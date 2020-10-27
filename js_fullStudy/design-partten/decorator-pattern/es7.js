8f99f3268c671aaacec724b6398294d5de9880f3
0173916 <1257668543@qq.com>
2020-10-27 21:12:51 +0800

    装饰器模式

---
+++ js_fullStudy/design-partten/decorator-pattern/es7.js
@@ -0,0 +1,13 @@
+function classDecorator(target) {
+  target.hasDecorator = true
+  return target
+}
+
+
+@classDecorator // classDecorator(Button)
+class Button {
+  
+}
+
+
+console.log('Button是否被装饰了:', Button.hasDecorator);
