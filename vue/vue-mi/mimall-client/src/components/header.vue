8c4bf4c8c2263f153bda81323a9ac4e9730110f8
0173916 <1257668543@qq.com>
2020-11-02 21:31:53 +0800

    客户端头部组件

---
+++ vue/vue-mi/mimall-client/src/components/header.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="wrap">
+    <div class="top">
+      <div class="container">
+        <div class="fr">
+          <div class="fl item user">
+            <div class="login">
+              <a href="" @mouseover="highLight" @mouseleave="fade" class="log"
+                >登录</a
+              >
+              <a href="" @mouseover="highLight" @mouseleave="fade" class="reg"
+                >注册</a
+              >
+            </div>
+          </div>
+          <span class="fl line"></span>
+          <div class="fl help">
+            <a href="https://www.xiaomiyoupin.com/help" class="m-safe-anchor"
+              >帮助中心</a
+            >
+          </div>
+          <span class="fl line"></span>
+          <div class="fl m-download">
+            <a href="https://www.xiaomiyoupin.com/download" class="download">
+              <img src="@/assets/Header-icon/phone.png" class="p-icon" />
+              下载APP
+            </a>
+          </div>
+          <span class="fl line"></span>
+          <div class="fl clauses" @mousemove="showNav" @mouseleave="hideNav">
+            <div class="m-clauses">
+              <a href="#" class="m-safe">资质证照&nbsp;/&nbsp;协议规则</a>
+              <a-icon type="down" class="icon-down" />
+            </div>
+            <div class="clauses-nav">
+              <ul class="nav-list">
+                <li>
+                  <a href="" class="quality">资质证照</a>
+                </li>
+                <li>
+                  <a href="" class="rules">协议规则</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clausesNav: false,
+    };
+  },
+  methods: {
+    highLight() {},
+    fade() {},
+    showNav() {
+      this.clausesNav = true;
+    },
+    hideNav() {
+      this.clausesNav = false;
+    },
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.top,
+.top a {
+  color: #e7e7e7;
+}
+.top {
+  height: 46px;
+  background: #333;
+  font-size: 14px;
+  position: relative;
+  z-index: 105;
+}
+.container {
+  width: 1080px;
+  margin: 0 auto;
+}
+.fr {
+  float: right;
+}
+.item {
+  position: relative;
+  margin-left: 5px;
+  text-align: center;
+}
+.fl {
+  float: left;
+}
+.line {
+  margin: 17px 3px 0 6px;
+  display: inline-block;
+  height: 16px;
+  vertical-align: middle;
+  border-right: 1px solid #666;
+}
+.help {
+  position: relative;
+  text-align: center;
+  height: 48px;
+  line-height: 48px;
+  margin-right: 8px;
+  margin-left: 11px;
+}
+.m-download {
+  position: relative;
+  margin-left: 5px;
+  text-align: center;
+}
+.download {
+  text-align: left;
+  left: 0;
+  padding-left: 30px;
+  display: block;
+  height: 48px;
+  line-height: 48px;
+  overflow: hidden;
+  margin-right: 5px;
+  position: relative;
+}
+.p-icon {
+  left: 1px;
+  top: 13px;
+  position: absolute;
+  width: 22px;
+  height: 22px;
+}
+.clauses {
+  position: relative;
+  text-align: center;
+  padding-right: 40px;
+  padding-left: 11px;
+}
+.m-clauses {
+  height: 48px;
+  line-height: 48px;
+  text-align: center;
+}
+.icon-down {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 16px;
+  right: 10px;
+}
+.clauses-nav {
+  width: 160px;
+  position: absolute;
+  left: 0;
+  top: 48px;
+  display: block;
+  overflow: hidden;
+  box-shadow: 0 3px 28px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s cubic-bezier(0, 1, 0.5, 1);
+  background: #fff;
+}
+.nva-list {
+  background-color: #fff;
+  list-style: none;
+  li {
+    height: 35px;
+    line-height: 35px;
+    padding: 0 10px;
+    a {
+      display: block;
+      font-size: 14px;
+      color: #666;
+    }
+  }
+}
+.login {
+  margin-right: 8px;
+  height: 48px;
+  line-height: 48px;
+  a {
+    margin-left: 8px;
+  }
+}
+</style>
--- vue/vue-mi/mimall-client/src/views/home/index.vue
+++ vue/vue-mi/mimall-client/src/views/home/index.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="home-wrap">
-    <div class="m-site-top">
-      <div class="container clearfix">
-        <div class="fr">
-          <div class="fl site-item m-user-con">
-            <div class="m-no-login">
-              <a href="https://account.xiaomi.com/pass/serviceLogin?sid=miotstore&_bannerBiz=miotstore&callback=https%3A%2F%2Fshopapi.io.mi.com%2Fapp%2Fshop%2Fauth%3Flogid%3D1602169190.811269641%26sign%3Db3d43fa56c93f5725a4d7e0825f8cb0a%26followup%3Dhttps%253A%252F%252Fwww.xiaomiyoupin.com%252F%253Fopapp%253D5%2526source%253Dyiqifa_19651%25257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%25253D%25253D_3.yiqifa.19651%25257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%25253D%25253D.1602252595199" class="m-safe-anchor">登录</a>
-              <a href="https://cn.account.xiaomi.com/pass/register?followup=https%3A%2F%2Fwww.xiaomiyoupin.com%2F%3Fopapp%3D5%26source%3Dyiqifa_19651%257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%253D%253D_3.yiqifa.19651%257CMTAyMTM4OHwwMDk3Nzg2OTIwMjhjYjAyOTczYQ%253D%253D.1602255603390" class="m-safe-anchor">注册</a>
-            </div>
-          </div>
-          <span class="fl m-line"></span>
-          <div class="fl m-help"></div>
-          <span class="fl m-line"></span>
-          <div class="fl m-download site-item"></div>
-          <span class="fl m-line"></span>
-          <div class="fl m-clauses-con"></div>
-        </div>
-      </div>
-    </div>
+  <!-- 首页容器 -->
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from '@/components/header.vue'
 export default {
-
-}
+  components: {
+    Header
+  }
+};
 </script>
 
-<style lang="stylus" scoped>
-
+<style lang="less" scoped>
 </style>
