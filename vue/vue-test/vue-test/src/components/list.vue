c813c33589baca2b69adc5d45e282e1474f132e8
0173916 <1257668543@qq.com>
2020-11-04 17:11:55 +0800

    测试index作为循环中key的缺点

---
+++ vue/vue-test/vue-test/src/components/list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="reverse">reverse</button>
+    <div v-for="(item, i) in lists" :key="i">
+      {{ item.content }}
+      <input type="text"/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      lists: [{ content: "0" }, { content: "1" }, { content: "2" }],
+    };
+  },
+  methods: {
+    reverse() {
+      this.lists = this.lists.reverse()
+    }
+  },
+};
+</script>
