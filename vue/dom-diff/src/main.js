bb81c9439492d27446e18ce059b45ef0bc28978e
0173916 <1257668543@qq.com>
2020-11-06 17:46:08 +0800

    diff算法

--- vue/dom-diff/src/main.js
+++ vue/dom-diff/src/main.js
@@ -1,15 +1,29 @@
 import { createElement, render, renderDom, Element } from './element'
+import diff from './diff'
+import patch from './patch'
 
 let virtualDom1 = createElement('ul', {class: 'list'}, [
   createElement('li', {class: 'item'}, ['1']),
-  createElement('li', {class: 'item'}, ['2'])
+  createElement('li', {class: 'item'}, ['2']),
+  createElement('li', {class: 'item'}, ['3'])
 ])
+
+let virtualDom2 = createElement('ul', {class: 'list-group', id: 'group'}, [
+  createElement('li', {class: 'item'}, ['a']),
+  createElement('li', {class: 'item'}, ['b']),
+  createElement('button', {class: 'item'}, ['c'])
+])
+
 
 let el = render(virtualDom1)
 renderDom(el, window.app)
 
-console.log(el);
-console.log(virtualDom1);
+
+let patches = diff(virtualDom1, virtualDom2) // 补丁
+console.log(patches);
+// 给元素打补丁 更新视图
+patch(el, patches)
+
 
 // virtualDom1 = <ul class="list">
 //                 <li class="item">1</li>
