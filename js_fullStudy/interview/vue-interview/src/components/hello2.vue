9c3f51c7b8b2d12aba8b201a29be7152156cbce0
0173916 <1257668543@qq.com>
2020-11-27 22:04:26 +0800

    vue-electron

--- js_fullStudy/interview/vue-interview/src/components/hello2.vue
+++ js_fullStudy/interview/vue-interview/src/components/hello2.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
     2- {{message}}
+    <div class="wrap" ref="wrap"></div>
   </div>
 </template>
 
 <script>
 export default {
-  inject: ['message']
+  inject: ['message'],
+  created() {
+    this.$nextTick(() => {
+      console.log(this.$refs.wrap);
+    })
+  }
 }
 </script>
 
