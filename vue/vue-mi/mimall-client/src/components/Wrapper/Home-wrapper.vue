f4dff2e419e9f69bf7cc64d7890c236c44c0e8d6
0173916 <1257668543@qq.com>
2020-12-07 21:17:22 +0800

    页面组件复用，主页二级路由

---
+++ vue/vue-mi/mimall-client/src/components/Wrapper/Home-wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Banner_nav />
+    <Classify_section />
+  </div>
+</template>
+
+<script>
+import Banner_nav from '@/components/home-components/Banner-nav.vue'
+import Classify_section from '@/components/home-components/Classify-section.vue'
+export default {
+  components: {
+    Banner_nav,
+    Classify_section
+  }
+}
+</script>
+
+<style>
+
+</style>
